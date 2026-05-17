326b1588e66db8ed09fbae1fb42026820602d1f5
Jirakrit <jayjirakrit@gmail.com>
2026-05-17 23:32:45 +0700

    [ADD] init SEO for Google

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-
+  site: "https://chpaisarn-website.vercel.app",
+  integrations: [
+    react(),
+    sitemap(),
+  ],
   vite: {
     plugins: [tailwindcss()],
     resolve: {
       alias: {
-        '@': '/src'
-      }
-    }
-  }
+        "@": "/src",
+      },
+    },
+  },
 });
